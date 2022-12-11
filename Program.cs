4b4e33ccfed3357bd40b0787d13dc13814158351
SourChips <souravchakraburtty@outlook.com>
2022-12-11 22:07:39 +0100

    code checkin

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution {
+    public bool IsValid(string s) {        
+    int length = s.Length;
+    Stack<char> stack = new Stack<char>();
+    Dictionary<char, char> dict = new Dictionary<char, char>();
+    dict.Add(')','(');
+    dict.Add(']','[');
+    dict.Add('}','{');    
+    for(int i=0; i < length; i++){
+        if(dict.ContainsKey(s[i])){            
+           if(stack.Any() && stack.Peek() == dict[s[i]]){
+               stack.Pop();
+           } 
+           else{
+               return false;
+           }           
+        }
+        else {
+            stack.Push(s[i]);
+        }
+    }
+    if(stack.Any()) return false; else return true;
+    }
+}
